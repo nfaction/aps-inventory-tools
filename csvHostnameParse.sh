b7a8978844f54b399d6b5aa672c17d34752d9647
wzandler <wjzandler@gmail.com>
2015-01-28 14:55:59 -0700

    Linux Hosts added

--- csvHostnameParse.sh
+++ csvHostnameParse.sh
@@ -22,7 +22,7 @@ inputFile=$1
 cat inventory.csv | cut -f 3 -d ',' | tail -n +2 
 
 # Find Mac hostnames
-cat inventory.csv | cut -f 3,16 -d ',' | tail -n +2 | grep -i "10.*" | grep -v -i "Ubuntu" >> MacHosts
+(cat inventory.csv | cut -f 3,16 -d ',' | tail -n +2 | grep -i "10.*" | grep -v -i "Ubuntu") >> MacHosts
 
 # Find Linux hostnames
-cat inventory.csv | cut -f 3,16 -d ',' | tail -n +2 | grep -i "10.*" | grep	-v
+cat inventory.csv | cut -f 3,16 -d ',' | tail -n +2 | grep -i "10.*" | grep -v -f MacHosts
